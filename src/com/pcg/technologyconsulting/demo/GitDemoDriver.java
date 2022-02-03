373de7daf300d16c80a982204b073514fee349cb
Peter Fotopoulos <pfotopoulos@pcgus.com>
2022-02-03 12:49:33 -0800

    Initial commit

---
+++ src/com/pcg/technologyconsulting/demo/GitDemoDriver.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.pcg.technologyconsulting.demo;
+
+/**
+ * @author pfotopoulos
+ *
+ */
+public class GitDemoDriver {
+
+	/**
+	 * Default Constructor
+	 */
+	public GitDemoDriver() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Driver has started!");
+	}
+
+}
